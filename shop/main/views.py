e8c8528315b5f43a255e42e1fe6ea711a40dbf74
ya-minion <108278614+meeinssq@users.noreply.github.com>
2026-02-20 23:15:17 +0500

    форма регистрации

--- shop/main/views.py
+++ shop/main/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
+from django.contrib.auth import login
 from django.http import HttpResponse
+
+
 def index(request):
     return render(request,"index.html")
 def cart(request):
     return render(request, "cart.html")
 def profile(request):
     return render(request, "profile.html")
+def forms(request):
+    return render(request, "forms.html")
