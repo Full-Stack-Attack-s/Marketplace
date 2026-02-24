e432d2efd52a784a645adec429f6fffa7e079c7e
Norv1L <sega0126.nt@gmail.com>
2026-02-24 13:54:28 +0500

    Применил gitignore, удалил мусор

--- shop/main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("cart/", views.cart, name="cart"),
-    path("profile/", views.profile, name="profile"),
-]
