9831dd2986e49da8a575df4a132a36cab7e07645
Norv1L <sega0126.nt@gmail.com>
2026-04-09 18:37:38 +0500

    Update views.py

--- shop/main/views.py
+++ shop/main/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
 
-from .forms import AddressForm, ProductForm, UserProfileEditForm
+from .forms import AddressForm, ProductForm, UserProfileEditForm,  StoreVerificationForm, UserProfileForm
 from .models import Product_variants, Product_images, OrderItems, ProductAttributeValues, Products, UserProfiles, \
     models, CategoryAttributes, Addresses, Stocks, Warehouses, StoreProfiles, Carts, \
     CartItems  # Обязательно добавь этот импорт!
